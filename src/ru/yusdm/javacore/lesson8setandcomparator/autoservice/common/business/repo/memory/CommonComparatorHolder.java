373a14dd3ec9d34aa5ffe3dfb6a920edf854dd7f
DmitryYusupov <usikovich@mail.ru>
2019-03-11 13:35:32 +0300

    Formated code

--- src/ru/yusdm/javacore/lesson8setandcomparator/autoservice/common/business/repo/memory/CommonComparatorHolder.java
+++ src/ru/yusdm/javacore/lesson8setandcomparator/autoservice/common/business/repo/memory/CommonComparatorHolder.java
@@ -8,22 +8,22 @@ public final class CommonComparatorHolder {
         @Override
         public int compare(String s1, String s2) {
 
-            if (s1 == null && s2 == null){
+            if (s1 == null && s2 == null) {
                 return 0;
-            }else if (s1!=null){
+            } else if (s1 != null) {
                 return s1.compareTo(s2);
-            }else {
+            } else {
                 return -1;
             }
 
         }
     };
 
-    private CommonComparatorHolder(){
+    private CommonComparatorHolder() {
 
     }
 
-    public static Comparator<String> getComparatorForNullableStrings(){
+    public static Comparator<String> getComparatorForNullableStrings() {
         return comparatorForNullableStrings;
     }
 
