8d1fa9a5eadc56e3d0b9621d8b13147863761d30
DmitryYusupov <usikovich@mail.ru>
2019-04-08 10:36:55 +0300

    Added dto's

---
+++ src/main/java/ru/yusdm/javacore/lesson21regularexpressions/autoservice/model/dto/ModelDto.java
@@ -0,0 +1,56 @@
+package ru.yusdm.javacore.lesson21regularexpressions.autoservice.model.dto;
+
+import ru.yusdm.javacore.lesson21regularexpressions.autoservice.common.business.dto.BaseDto;
+import ru.yusdm.javacore.lesson21regularexpressions.autoservice.model.domain.ModelDiscriminator;
+
+public abstract class ModelDto extends BaseDto<Long> {
+
+    protected String name;
+    protected String description;
+    protected int productionYearStart;
+    protected Integer productionYearEnd;
+    protected ModelDiscriminator discriminator;
+
+    public ModelDto() {
+        initDiscriminator();
+    }
+
+    protected abstract void initDiscriminator();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getProductionYearStart() {
+        return productionYearStart;
+    }
+
+    public void setProductionYearStart(int productionYearStart) {
+        this.productionYearStart = productionYearStart;
+    }
+
+    public Integer getProductionYearEnd() {
+        return productionYearEnd;
+    }
+
+    public void setProductionYearEnd(Integer productionYearEnd) {
+        this.productionYearEnd = productionYearEnd;
+    }
+
+    public ModelDiscriminator getDiscriminator() {
+        return discriminator;
+    }
+
+}
